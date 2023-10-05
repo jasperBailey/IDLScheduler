5104440f6a76c525040eed5d461da23fd35029f4
Jasper Bailey <jasperbailey98@gmail.com>
2023-10-05 17:03:50 +0100

    implemented DFS for perfect solutions

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 
+
 class Todo(tk.Tk):
     def __init__(self, tasks=None):
         super().__init__()
@@ -12,8 +13,10 @@ class Todo(tk.Tk):
         self.title("To-Do App v1")
         self.geometry("300x400")
 
-        todo1 = tk.Label(self, text="--- Add Items Here ---", bg="lightgrey", fg="black", pady=10)
-        
+        todo1 = tk.Label(
+            self, text="--- Add Items Here ---", bg="lightgrey", fg="black", pady=10
+        )
+
         self.tasks.append(todo1)
 
         for task in self.tasks:
@@ -26,10 +29,13 @@ class Todo(tk.Tk):
 
         self.bind("<Return>", self.add_task)
 
-        self.colour_schemes = [{"bg": "lightgrey", "fg": "black"}, {"bg": "grey", "fg": "white"}]
+        self.colour_schemes = [
+            {"bg": "lightgrey", "fg": "black"},
+            {"bg": "grey", "fg": "white"},
+        ]
 
     def add_task(self, event=None):
-        task_text = self.task_create.get(1.0,tk.END).strip()
+        task_text = self.task_create.get(1.0, tk.END).strip()
 
         if len(task_text) > 0:
             new_task = tk.Label(self, text=task_text, pady=10)
@@ -46,6 +52,7 @@ class Todo(tk.Tk):
             self.tasks.append(new_task)
 
         self.task_create.delete(1.0, tk.END)
+
 
 if __name__ == "__main__":
     todo = Todo()
--- stuff.py
+++ stuff.py
@@ -1,39 +1,112 @@
 import csv
 
-teamNames = []
-teamData = []
+teamData = [[], []]
+numTeams = 8
+numWeeks = numTeams - 1
 
-with open('data.csv', newline='') as csvfile:
+with open("data.csv", newline="") as csvfile:
     spamreader = csv.reader(csvfile)
+
     for row in spamreader:
-        teamNames.append(row[0])
+        teamData[0].append(row[0])
+
         weekData = []
-        for i in range(7):
-            weekData.append(row[7*i+1:7*i+8])
-        teamData.append(weekData)
 
-possibleMatches = []
-matchupTable = []
+        for i in range(numWeeks):
+            weekData.append(row[7 * i + 1 : 7 * i + 8])
 
-for team1 in range(8):
-    for team2 in range(team1, 8):
+        teamData[1].append(weekData)
+
+
+pairingsData = []
+
+# create list of possible matchups and list of matchup availabilities
+# in each time period (week)
+for team1 in range(numTeams):
+    for team2 in range(team1, numTeams):
         if team1 == team2:
             continue
-        matchupData = []
-        matchupTable.append( {
-            teamNames[team1],
-            teamNames[team2]
-        })
-        for week in range(7):
+
+        matchupData = [frozenset([teamData[0][team1], teamData[0][team2]]), []]
+
+        for week in range(numWeeks):
             isWeekPossible = False
+
             for day in range(7):
-                if float(teamData[team1][week][day]) + float(teamData[team2][week][day]) == 10:
+                if (
+                    float(teamData[1][team1][week][day])
+                    + float(teamData[1][team2][week][day])
+                    == 10
+                ):
                     isWeekPossible = True
                     break
-            matchupData.append(isWeekPossible)
-        possibleMatches.append(matchupData)
-    
+
+            matchupData[1].append(isWeekPossible)
+
+        pairingsData.append(matchupData)
+
+# order matchups by number of possible weeks
+pairingsData.sort(key=lambda e: e[1].count(True))
+print(pairingsData)
 
 
-print(matchupTable)
-print(possibleMatches)
+def fillScheduleDFS(curSolution=None, depth=0):
+    # Parameters:
+    #
+    # curSolution: List | None - the current progress on the curSolution, as
+    #     a list of sets, with curSolution[x] giving the set of pairings on week x
+    #
+    # depth: int - the recursion depth, equal to the number of pairings
+    #     placed in curSolution
+    #
+    # Returns:
+    #
+    # List | None - The full schedule, if there is one, or None otherwise
+
+    # set empty solution if just starting
+    if curSolution == None:
+        curSolution = []
+        # create an empty set to hold pairings for the week
+        for i in range(numWeeks):
+            curSolution.append(set())
+
+    pairingToPlace = pairingsData[depth]
+
+    for i in range(numWeeks):
+        # check if the pairing can happen in that week
+        if not pairingToPlace[1][i]:
+            continue
+
+        # check if neither team already has a match that week
+        bothTeamsFree = True
+        for pairing in curSolution[i]:
+            if not pairing.isdisjoint(pairingToPlace[0]):
+                bothTeamsFree = False
+                break
+
+        if bothTeamsFree:
+            curSolution[i].add(pairingToPlace[0])
+
+            # check if solution is now finished
+            solutionFinished = True
+            for week in curSolution:
+                if len(week) != numTeams / 2:
+                    solutionFinished = False
+                    break
+
+            if solutionFinished:
+                return curSolution
+            else:
+                nextSol = fillScheduleDFS(curSolution, depth + 1)
+                if nextSol != None:
+                    return nextSol
+                else:
+                    # if the next iteration couldn't find a valid solution,
+                    # remove the last pairing and continue trying to place
+                    # on subsequent weeks
+                    curSolution[i].remove(pairingToPlace[0])
+    print("cannot place pairing:", pairingToPlace, "at depth", depth)
+    print("current solution:", curSolution)
+
+
+print(fillScheduleDFS())
