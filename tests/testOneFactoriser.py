de2d5aa01066ec315f79e0ef9ea07b6c22677d58
Jasper Bailey <jasperbailey98@gmail.com>
2023-11-07 15:46:43 +0000

    gen base 1factors and convert to lists to apply mappings

--- tests/testOneFactoriser.py
+++ tests/testOneFactoriser.py
@@ -5,14 +5,30 @@ from models.onefactoriser import OneFactoriser
 class TestOneFactoriser(unittest.TestCase):
     def setUp(self):
         self.onefact = OneFactoriser(8)
+        self.onefact.oneFactorisations()
 
     @unittest.skip
     def testCreateEdges(self):
-        print(self.onefact.getEdges())
+        print(f"edges: {self.onefact.getEdges()}")
 
     @unittest.skip
     def testInitialSol(self):
         print(self.onefact.getInitialSol())
 
+    @unittest.skip
     def testGetOneFactors(self):
         print(self.onefact.getOneFactors())
+
+    @unittest.skip
+    def testGetOneFactorisations(self):
+        print(self.onefact.getOneFactorisations()[0])
+
+    @unittest.skip
+    def testGetW1(self):
+        for thingy in self.onefact.getW1():
+            print(thingy)
+        # print(self.onefact.getOneFactorisations()[0])
+
+    # @unittest.skip
+    def testGetOneFactorisationsAsLists(self):
+        print(self.onefact.getOneFactorisationsAsLists()[0])
